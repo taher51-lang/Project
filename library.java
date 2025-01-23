195758765f8cf6fbef3b6c757af010c064dee5db
taher51-lang <rangwalataher17@gmail.com>
2025-01-23 21:04:39 +0530

    Display methods for book including genre fiction

--- library.java
+++ library.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
-
-public class library {}
-    class Main12 {
+   public class library {
+        Scanner sc = new Scanner(System.in);
         boolean login(String username, String pass, Members obj){
             if(username.equalsIgnoreCase(obj.name)&&pass.equals(obj.defaultpass)){
                 return true;
@@ -12,22 +11,39 @@ public class library {}
             }
         }
     }
-    
-    class Reader {
-
+    class Librarian {
     }
-    class Books {
-        Scanner sc = new Scanner(System.in);
-        String bookName, bookauhtor;
-        int bookPrice;
+    class Books extends library  {
+        String bookName,bookauhtor,Genre;
+        int bookId;
+        static int id=1,defaultStock = 5;
+        int current_Stock = defaultStock;
+        Books(int k){}
         Books() {
-            bookName = sc.nextLine();
-            bookauhtor = sc.nextLine();
-            bookPrice = sc.nextInt();
-            sc.nextLine();
+            bookId=id++;
+        }
+        void displayBooks(Books array[],String genre){
+            for(int i=0;i< array.length;i++){
+                if(genre.equalsIgnoreCase(array[i].Genre)){
+                System.out.print(array[i].bookName+" "+array[i].bookId);
+                System.out.println(array[i].bookauhtor);}
+            }
+            System.out.println("Kindly enter the book id You want to read");
+            byte userBookId = sc.nextByte();
+            if(availibility(userBookId,array)){
+                System.out.println("This Book has been issued to you");
+            }
+        }
+        boolean availibility(int userinput,Books array[]){//Stock availibilty
+            if(array[userinput-1].current_Stock>0){
+                array[userinput-1].current_Stock-=1;
+                return true;
+            }
+            else
+                return false;
         }
     }
-    class Members {
+    class Members extends library{
         Scanner sc = new Scanner(System.in);
         String name,defaultpass;
         int penalty,id;
@@ -36,35 +52,63 @@ public class library {}
             name = sc.nextLine();defaultpass="1";
         }
     }
-
     class Run123 {
         public static void main(String args[]) {
             Scanner sc = new Scanner(System.in);
-            Books books[] = new Books[10];
-            for (int i = 0; i < books.length; i++) {
-                //books[i] = new Books();
+            Books books[] = new Books[2];
+            Books callBook = new Books(1);
+            for (int i = 0; i < books.length; i++) {//provides the array of books
+                books[i] = new Books();
             }
-            Members members[] = new Members[1];
-            for(int i =0;i< members.length;i++){
+            Members members[] = new Members[1];//Provides the array of members
+            for (int i = 0; i < members.length; i++) {
                 members[i] = new Members();
             }
-            int attempt=0;boolean loginStat = false;
-            Main12 callobj = new Main12();
-            while(true) {
-                System.out.println("Enter Your name , pass and id respectively");
-                if (callobj.login(sc.nextLine(), sc.nextLine(), members[sc.nextInt() - 1])) {
-                    System.out.println("Login Success");loginStat=true;break;
-                }
-                else if (attempt==3) {
-                    System.out.println("Your account has been Locked");
-                    break;
-                }
-                else {attempt++;
-                    continue;
-                }
-            }
-            if(loginStat){
-                //code continues here;
+            System.out.println("Type Reader if you are a reader and librarian if you are a librarian");
+            String userType = sc.nextLine();
+            int type = userType.equalsIgnoreCase("Reader") ? 1 : 2;
+            switch (type) {
+                case 1:
+                    System.out.println("Are you an existing member of library?Yes/No");
+                    String userClass = sc.nextLine();
+                    if(userClass.equalsIgnoreCase("Yes")) {
+                        int attempt = 0;
+                        boolean loginStat = false;
+                        library callobj = new library();
+                        while (true) {//Login
+                            System.out.println("Enter Your name , pass and id respectively");
+                            if (callobj.login(sc.nextLine(), sc.nextLine(), members[sc.nextInt() - 1])) {
+                                System.out.println("Login Success");
+                                loginStat = true;
+                                break;
+                            } else if (attempt == 3) {
+                                System.out.println("Your account has been Locked");
+                                break;
+                            } else {
+                                attempt++;
+                            }
+                        }
+                        if (loginStat) {
+                            System.out.println("Select your preferred Genre of Books:");
+                            System.out.println("1.Fiction\n2.Horror\nPhysocological");
+                            byte userGenre = sc.nextByte();
+                            switch (userGenre){
+                                case 1:callBook.displayBooks(books,"Fiction");break;
+                                case 2:callBook.displayBooks(books,"Horror");break;
+                                case 3:callBook.displayBooks(books,"Physocological");break;
+                            }
+                            System.out.println("Happy Reading");
+                            callBook.displayBooks(books,"Fiction");
+                            //code continues here;
+                        }
+                    }
+                    else {//registering a new member
+                        System.out.println("We will be glad to have you as our member.");
+                        System.out.println("Please enter details below for registering as a new Member");
+                    }
+                    break;//Member operations end
+            case 2 :
+
             }
         }
     }
