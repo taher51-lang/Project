acb527bf545f6227a196190a118b45181f1788fa
taher51-lang <rangwalataher17@gmail.com>
2025-07-16 22:42:14 +0530

    More progress

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner sc = new Scanner(System.in);
-
     static void roleIdentification() throws SQLException, ClassNotFoundException {
         System.out.println("---Kindly choose your specific role");
         System.out.println("---1.User");
@@ -33,7 +32,7 @@ public class Main {
                         case 1: login();
                             temp=false;
                             break;
-                        case 2: registration();
+                        case 2: User.registration();
                             temp=false;
                             break;
                         default:
@@ -51,9 +50,22 @@ public class Main {
     static boolean login(){
         return true;
     }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to the lost and found Managment System ");
+        roleIdentification();
+    }
+}
+class User{
+    static Scanner sc = new Scanner(System.in);
+    String username;
+    String password;
+    Long MobileNo;
+    String email_id;
+    String Name;
     static void registration() throws ClassNotFoundException, SQLException {
         System.out.println("----Follow the steps to register yourself ");
-        System.out.println("Enter your name");sc.nextLine();
+        System.out.println("Enter your name");
         String Name = sc.nextLine();
         System.out.println("Create a username for your profile");
         String username = sc.next();
@@ -63,7 +75,7 @@ public class Main {
         long mobileNo = 0;
         boolean temp=true;
         while (temp){
-           try{
+            try{
                 mobileNo=sc.nextLong();
                 if(mobileNo<=9999999999L&&mobileNo>7999999999L){
                     temp=false;
@@ -71,10 +83,10 @@ public class Main {
                     System.out.println("invalid input");
                 }
             }
-           catch (RuntimeException e) {
-               System.out.println("Enter numeric input");
-               sc.nextLine();
-           }
+            catch (RuntimeException e) {
+                System.out.println("Enter numeric input");
+                sc.nextLine();
+            }
         }
         System.out.println("Enter your email address");
         String email_id = sc.next();
@@ -91,18 +103,6 @@ public class Main {
         int r = st.executeUpdate(sql1);
 
     }
-    public static void main(String[] args) throws Exception {
-        System.out.println("Welcome to the lost and found Managment System ");
-        roleIdentification();
-    }
-}
-class User{
-    String username;
-    String password;
-    Long MobileNo;
-    String email_id;
-    String Name;
-    public void regitration(){}
 
     public void setEmail_id(String email_id) {
         this.email_id = email_id;
