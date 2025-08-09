f3c5bc94a820cd05d0668879fbed72471ea58636
taher51-lang <rangwalataher17@gmail.com>
2025-08-09 23:44:55 +0530

    Found insertion added. Table ready for filtering

--- Main.java
+++ Main.java
@@ -98,14 +98,9 @@ public class Main {
                         case 1: while (!login()){
                             System.out.println("Incorrect userName or password");
                         }
-                            System.out.println("Enter whether you have lost Or found any thing");
-                            String circumstance=sc.next();
-                            if(circumstance.equalsIgnoreCase("lost")){
-                                Lost();
-                            }
-                            temp = false;//Updates of User side dashboard to be made here
-                            break;
+                        LostAndFoundProcess();
                         case 2: User.registration();
+                        LostAndFoundProcess();
                             temp=false;
                             break;
                         default:
@@ -139,6 +134,16 @@ public class Main {
             return true;
         }
         return false;
+    }
+    static void LostAndFoundProcess() throws SQLException {
+        System.out.println("Enter whether you have lost Or found any thing");
+        String circumstance=sc.next();
+        if(circumstance.equalsIgnoreCase("lost")){
+            Lost();
+        }
+        else{
+            found();
+        }
     }
     static public int insertDetails(Item e) throws SQLException {
         con = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "");
@@ -213,7 +218,6 @@ public class Main {
             }
             isValid=false;
         }
-        Statement st = con.createStatement();
         switch (choice){
             case 1:Electronics e = new Electronics();
                 e.setDetails();
@@ -307,26 +311,148 @@ public class Main {
         }
         return lost;
     }
-//    static void found(){
-//        System.out.println("Kindly Enter the details of the thing you have found as instructed");
-//        System.out.println("what is the thing you have found(e.g wallet,Mobile etc)?");
-//        String type = sc.nextLine();
-//        System.out.println("Enter the area you have found it");
-//        String area = sc.nextLine();
-//        System.out.println("Enter the date(YYYY-MM-DD) ");
-//        String date=sc.nextLine();
-//        System.out.println("Enter it's color");
-//        String color=sc.nextLine();
-//        System.out.println("Enter it's brand");
-//        String brand=sc.nextLine();
-//        System.out.println("Enter (Yes/No) there is a unique attribute");
-//        String choice=sc.nextLine();
-//    }
+    static void found() throws SQLException {
+        System.out.println("Kindly Enter the details of the thing you have found as instructed");
+        System.out.println("Kindly enter the category in which item you found fits perfectly ");
+        System.out.println("""
+                1.Electronics & Gadgets
+                (Mobile phones, tablets, laptops, cameras, chargers, earphones etc)
+                
+                
+                2.Bags & Carriers
+                (Suitcases, backpacks, laptop bags, tote bags)
+                
+                
+                3.Clothing & Accessories
+                (Jackets, scarves, gloves, caps, jewelry, watches)
+                
+                
+                4.Identity & Documents
+                (ID cards, passports, licenses, student cards, certificates)
+                
+                
+                5.Academic Supplies
+                Books, notebooks, stationery, calculators
+                
+                
+                6.Hobby & Entertainment Gear
+                
+                (Game consoles, musical instruments, sports equipment)
+                
+                
+                .7Children’s Belongings
+                
+                (Toys, lunch boxes, baby bags, pacifiers)
+                
+                8.Eyewear & Vision Aids
+                
+                (Glasses, sunglasses, contact lens kits)
+                
+                
+                9.Keys & Access Devices
+                
+                (House keys, car keys, RFID tags, access cards)""");
+        int choice = 0;
+        boolean isValid = true;
+        while (isValid){
+            try{
+                choice=Integer.parseInt(sc.next());
+            }
+            catch (Exception e){
+                System.out.println("Enter numeric input from the lost category menu");
+                continue;
+            }
+            isValid=false;
+        }
+        switch (choice){
+            case 1:Electronics e = new Electronics();
+                e.setDetails();
+                int lid1 = insertDetails(e);//Last generated id
+                PreparedStatement pst1 = con.prepareStatement("insert into electronicsandgadgets values(?,?,?)");
+                pst1.setInt(1,lid1);
+                pst1.setString(2,e.brand);
+                pst1.setString(3,e.model);
+                pst1.executeUpdate();
+                break;
+            case 2:Bags a = new Bags();
+                a.setDetails();
+                int lid2 = insertDetails(a);//Last generated id
+                PreparedStatement pst2 = con.prepareStatement("insert into bags values(?,?,?)");
+                pst2.setInt(1,lid2);
+                pst2.setString(2,a.material);
+                pst2.setString(3,a.brand);
+                pst2.executeUpdate();
+                break;
+            case 3: Accessories c = new Accessories();
+                c.setDetails();
+                int lid3 = insertDetails(c);
+                PreparedStatement pst3 = con.prepareStatement("insert into accessories values(?,?)");
+                pst3.setInt(1,lid3);
+                pst3.setString(2,c.brand);
+                pst3.executeUpdate();
+                break;
+            case 4:Documents d = new Documents();
+                d.setDetails();
+                int lid4 = insertDetails(d);
+                PreparedStatement pst4 = con.prepareStatement("insert into Documents values(?,?)");
+                pst4.setInt(1,lid4);
+                pst4.setString(2,d.issueAuthority);
+                pst4.executeUpdate();
+                break;
+            case 5:AcademicSupplies s = new AcademicSupplies();
+                s.setDetails();
+                int lid5 = insertDetails(s);
+                PreparedStatement pst5 = con.prepareStatement("insert into AcademicSupplies values(?,?)");
+                pst5.setInt(1,lid5);
+                pst5.setString(2,s.brand);
+                pst5.executeUpdate();
+                break;
+            case 6:EntertainmentsGears eg = new EntertainmentsGears();
+                eg.setDetails();
+                int lid6 = insertDetails(eg);
+                PreparedStatement pst6 = con.prepareStatement("insert into entertainmentgears values(?,?)");
+                pst6.setInt(1,lid6);
+                pst6.setString(2,eg.brand);
+                pst6.executeUpdate();
+                break;
+            case 7:ChildStuff cs = new ChildStuff();
+                cs.setDetails();
+                int lid7 = insertDetails(cs);
+                PreparedStatement pst7 = con.prepareStatement("insert into Childstuff values(?,?)");
+                pst7.setInt(1,lid7);
+                pst7.setString(2,cs.brand);
+                pst7.executeUpdate();
+                break;
+            case 8:eyeAndVision ev = new eyeAndVision();
+                ev.setDetails();
+                int lid8 = insertDetails(ev);
+                PreparedStatement pst8 = con.prepareStatement("insert into eyeAndvision values(?,?,?)");
+                pst8.setInt(1,lid8);
+                pst8.setString(2,ev.FrameType);
+                pst8.setString(3,ev.lensGrade);
+                pst8.setString(4,ev.brand);
+                pst8.executeUpdate();
+                break;
+            case 9:Keys k = new Keys();
+                k.setDetails();
+                int lid9 = insertDetails(k);
+                PreparedStatement pst9 = con.prepareStatement("insert into Keys values(?,?)");
+                pst9.setInt(1,lid9);
+                pst9.setString(2,k.keyType);
+                pst9.setString(3,k.brand);
+                pst9.executeUpdate();
+                break;
+            default:
+                System.out.println("Kindly enter proper input");
+                found();
+                break;
+        }
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to the lost and found Managment System ");
         if(roleIdentification()){
             System.out.println("Hoo");
-        };
+        }
         //User Login-User side function embedded in nested methods one after one in this method;
 
     }
@@ -362,7 +488,6 @@ class User{
         while (Users.insert(username)){
             username=sc.next();
         }
-        //Make sure this username has not been taken already
         System.out.println("Create new password");
         String pass = sc.next();
         System.out.println("Enter your mobile No(It must start with 8  or 9)");
@@ -386,6 +511,12 @@ class User{
         String email_id = sc.next();
         Statement st = con.createStatement();
         String sql1 = "insert into user(user_name,Name,mobileNo,email_id,user_pass) values('"+username+"','"+Name+"',"+mobileNo+",'"+email_id+"','"+pass+"')";
+        st.executeUpdate(sql1);
+        String sql2="Select userId,name from user where user_Name='"+username+"'";
+        Statement st2 = con.createStatement();
+        ResultSet rs2= st2.executeQuery(sql2);
+        if(rs2.next())
+            Main.id = rs2.getInt(1);
     }
     public void setEmail_id(String email_id) {
         this.email_id = email_id;
@@ -398,32 +529,27 @@ class Item {
     String date;
     String color;
     String attribute;
-
     public void setName() {
         name = sc.nextLine();
     }
-
     public void setArea() {
-        System.out.println("Enter the area it might have lost");
+        System.out.println("Enter the area it might have lost/found");
         area = sc.nextLine();
     }
-
     public void setDate() {
-        System.out.println("Enter the date when it got lost");
+        System.out.println("Enter the date when it got lost/found");
         try {
             date = sc.nextLine();
         } catch (Exception e) {
             System.out.println("enter proper date");
         }
     }
-
     public void setColor() {
-        System.out.println("Enter the color of the lost thing");
+        System.out.println("Enter the color of the lost/found thing");
         color = sc.nextLine();
     }
-
     public void setAttribute() {
-        System.out.println("Describe any unique quality of your lost item that helps the system to validate its rightful owner");
+        System.out.println("Describe any unique quality of your lost/found item that helps the system to validate its rightful owner");
         attribute =sc.nextLine();
     }
 }
@@ -431,7 +557,7 @@ class Electronics extends Item{
    String model;
    String brand;
     public void setBrand() {
-        System.out.println("Enter the brand of the lost item if any");
+        System.out.println("Enter the brand of the lost/found item if any");
         brand = sc.nextLine();
     }
    public void setDetails(){
@@ -445,13 +571,13 @@ class Bags extends Item{
   String material;
     String brand;
     public void setBrand() {
-        System.out.println("Enter the brand of the lost item if any");
+        System.out.println("Enter the brand of the lost/found item if any");
         brand = sc.nextLine();
     }
   public void setDetails(){
       System.out.println("Enter the category of bag: e.g(shoulder bag,suitcase etc)");
       setName();setDate();setArea();setBrand();setColor();
-      System.out.println("Enter the material of the lost bag");
+      System.out.println("Enter the material of the  bag");
       material=sc.nextLine();
       setAttribute();
   }
@@ -459,23 +585,22 @@ class Bags extends Item{
 class Accessories extends Item{
     String brand;
     public void setBrand() {
-        System.out.println("Enter the brand of the lost item if any");
+        System.out.println("Enter the brand of the  item if any");
         brand = sc.nextLine();
     }
     String material;
     public void setDetails(){
-        System.out.println("Enter the type of Accessory you lost. e.g(Watch,cloth type(jacket,sweater) etc)");
+        System.out.println("Enter the type of Accessory . e.g(Watch,cloth type(jacket,sweater) etc)");
         setName();setDate();setArea();setBrand();setColor();
-        System.out.println("Enter the material of the lost item:(In one word)");
+        System.out.println("Enter the material of the  item:(In one word)");
         material= sc.next();
-        System.out.println("Enter the condition of the Item(new,old)(In one word):");
         setAttribute();
     }
 }
 class Documents extends Item{
     String issueAuthority;
     public void setDetails(){
-        System.out.println("Enter the name of Document you lost. e.g(Aadhar card,Pan card etc)");
+        System.out.println("Enter the name of Document . e.g(Aadhar card,Pan card etc)");
         setName();setDate();setArea();setColor();
         System.out.println("Specify The issuing authority of the lost Document");
         issueAuthority=sc.nextLine();
